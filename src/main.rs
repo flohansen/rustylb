90f482dde6e7a3bae384010afa900d84c277fc61
Florian Hansen <flhansen.93@gmail.com>
2024-07-07 19:52:56 +0200

    feat(experimental): add load balancing targets

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::net::IpAddr;
+
 pub mod application;
 pub mod network;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    network::LoadBalancer::new()
+    let targets = vec![
+        network::Target::new(IpAddr::V4([127, 0, 0, 1].into()), 3001),
+    ];
+
+    network::LoadBalancer::new(targets)
         .run()
         .await
 }
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,15 +1,35 @@
-use tokio::net::{TcpListener, TcpStream};
+use std::{net::{IpAddr, SocketAddr}, sync::Arc, time::Duration};
+use tokio::{io::copy_bidirectional, net::{TcpListener, TcpStream}, time::timeout};
+
+pub struct Target {
+    ip: IpAddr,
+    port: u16,
+}
+
+impl Target {
+    pub fn new(ip: IpAddr, port: u16) -> Self {
+        Target { ip, port }
+    }
+}
 
 pub struct LoadBalancer {
+    targets: Vec<Target>,
 }
 
 impl LoadBalancer {
-    pub fn new() -> Self {
-        LoadBalancer {}
+    pub fn new(targets: Vec<Target>) -> Self {
+        LoadBalancer { targets }
     }
 
-    async fn handle_connection(_socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-        // TODO: implement load balancing algorithm
+    async fn handle_connection(&self, mut socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+        let addr = SocketAddr::from((self.targets[0].ip, self.targets[0].port));
+        let mut conn = TcpStream::connect(addr).await?;
+
+        let timeout_duration = Duration::from_secs(5);
+        if let Err(err) = timeout(timeout_duration, copy_bidirectional(&mut socket, &mut conn)).await {
+            eprintln!("Error proxying request: {}", err);
+        }
+
         Ok(())
     }
 
@@ -17,11 +37,16 @@ impl LoadBalancer {
         let addr = "0.0.0.0:3000";
         let listener = TcpListener::bind(addr).await?;
 
-        loop {
-            let (socket, _) = listener.accept().await?;
+        let arc = Arc::new(self);
 
-            tokio::spawn(async {
-                if let Err(err) = Self::handle_connection(socket).await {
+        loop {
+            let (socket, addr) = listener.accept().await?;
+
+            println!("Received request from {}", addr);
+
+            let lb = arc.clone();
+            tokio::spawn(async move {
+                if let Err(err) = lb.handle_connection(socket).await {
                     eprintln!("Error handling connection: {}", err);
                 }
             });
