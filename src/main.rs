2d39945d9964b64c2c6d1e08c86a34109e8a4a44
Florian Hansen <flhansen.93@gmail.com>
2024-07-07 23:27:49 +0200

    docs(experimental): add documentation comments

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,19 @@ pub mod strategy;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Hard coded targets to test the load balancer locally.
+    // TODO: Read configuration file or something.
     let targets = vec![
         network::Target::new(IpAddr::V4([127, 0, 0, 1].into()), 3001),
         network::Target::new(IpAddr::V4([127, 0, 0, 1].into()), 3002),
         network::Target::new(IpAddr::V4([127, 0, 0, 1].into()), 3003),
     ];
 
+    // The strategy used for distributing incoming TCP packets to the targets.
     let strategy = strategy::RoundRobin::new(targets);
 
+    // Create and run the load balancer.
+    // TODO: Prettify this e.g. by using builder pattern.
     network::LoadBalancer::new(strategy)
         .run()
         .await
--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,6 +1,7 @@
 use std::{net::{IpAddr, SocketAddr}, sync::Arc, time::Duration};
 use tokio::{io::copy_bidirectional, net::{TcpListener, TcpStream}, sync::Mutex};
 
+/// Represents a target server for load balancing.
 pub struct Target {
     ip: IpAddr,
     port: u16,
@@ -12,19 +13,26 @@ impl Target {
     }
 }
 
+/// Defines the interface for a load balancing strategy.
 pub trait BalancingStrategy: Send + Sync {
+    /// Selects the next target server based on the implemented strategy.
     fn next(&mut self) -> Option<&Target>;
 }
 
+/// A network load balancer that distributes incoming TCP connections to backend servers according
+/// to a chosen balancing strategy.
 pub struct LoadBalancer {
     strategy: Arc<Mutex<dyn BalancingStrategy>>,
 }
 
 impl LoadBalancer {
+    /// Creates a new `LoadBalancer` instance with the specified balancing strategy.
     pub fn new(strategy: impl BalancingStrategy + 'static) -> Self {
         LoadBalancer { strategy: Arc::new(Mutex::new(strategy)) }
     }
 
+    /// Handles an incoming TCP connection by forwarding it to a backend server selected by the
+    /// load balancing strategy.
     async fn handle_connection(&self, mut socket: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
         if let Some(target) = self.strategy.lock().await.next() {
             let addr = SocketAddr::from((target.ip, target.port));
@@ -49,6 +57,8 @@ impl LoadBalancer {
         }
     }
 
+    /// Starts the load balancer, listening on the specified address and accepting incoming
+    /// connections.
     pub async fn run(self) -> Result<(), Box<dyn std::error::Error>> {
         let addr = "0.0.0.0:3000";
         let listener = TcpListener::bind(addr).await?;
